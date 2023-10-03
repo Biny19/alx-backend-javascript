b8d6c79caa3f0552e4eaf8f3373a29eb9ae174a4
Biny19 <binalfew.tadele@gmail.com>
2023-10-03 06:05:53 +0000

    ES6 data manipulation js project

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+function getListStudents() {
+  const Student = [
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+  return Student;
+}
+
+export default getListStudents;
